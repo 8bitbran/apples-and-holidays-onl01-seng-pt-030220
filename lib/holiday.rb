3822126f8f4a270a4040420bbe534dc037d25ce8
Brandon Marrero <brandonmar97@gmail.com>
2020-03-13 23:36:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,8 +29,9 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |season, holiday|
     if season == :winter
       holiday.each do |day, data|
+        binding.pry
         holiday_hash[:winter][day][data] << supply
-
+        
       end
     end
   end
